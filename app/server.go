306e99d5d363036aefcdb65d4c1565963f5a48f1
Georg Stockinger <gstockinger@a2mac1.com>
2024-10-28 17:34:59 +0100

    send response

--- app/server.go
+++ app/server.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
+
+type KafkaResponse struct {
+	MessageSize   int32
+	CorrelationId int32
+}
+
+func (r *KafkaResponse) Encode() []byte {
+
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.BigEndian, r)
+	return buf.Bytes()
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -17,9 +31,25 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+		response := KafkaResponse{
+			MessageSize:   0,
+			CorrelationId: 7,
+		}
+		encoded := response.Encode()
+		_, err = conn.Write(encoded)
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
+		conn.Close()
+
 	}
 }
